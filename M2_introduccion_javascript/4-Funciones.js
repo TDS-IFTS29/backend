1cb3c78cff390a9caf271ad8a8731fda46c91784
Damian Clausi <damian.clausi@gmail.com>
2025-03-29 14:22:49 -0300

    Funciones

---
+++ M2_introduccion_javascript/4-Funciones.js
@@ -0,0 +1,41 @@
+// console.log('El futuro dice:', futuro());
+// function futuro() {
+//  return 'Nunca tendran autos voladores';
+// }
+
+//---------------------------------------
+        
+// const hacerSonido = function(){
+//     console.log('Pling!');
+// };
+// hacerSonido();
+
+// const potencia = function (base, exponente){
+//     let resultado = 1;
+//     for (let cuenta = 0 ; cuenta < exponente; cuenta ++){
+//         resultado * = base;
+//     }
+// }
+
+//FUNCIONES FLECHAS
+// EN LA GUIA HAY MAS EJEMPLOS DE COMO USAR DE FORMA ABREVIADA
+
+// const potencia = (base, exponente) => {
+//     let resultdo = 1;
+//     for (let cuenta = 0 ; cuenta < exponente; cuenta ++){
+//         resultdo *= base;        
+//     }
+//     return resultdo;
+// }
+// console.log(potencia(2,10));
+
+function potencia(base, exponente) {
+    if (exponente == 0) {
+      return 1;
+    } else {
+      return base * potencia(base, exponente - 1);
+    }
+   }
+   console.log(potencia(2, 3));
+   // → 8
+           
