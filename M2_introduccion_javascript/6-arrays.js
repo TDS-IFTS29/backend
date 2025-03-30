ac24e3cde32369de45e7b4aed4ebc22cf4dab265
Damian Clausi <damian.clausi@gmail.com>
2025-03-30 14:17:53 -0300

    arrays filtos

--- M2_introduccion_javascript/6-arrays.js
+++ M2_introduccion_javascript/6-arrays.js
@@ -6,3 +6,15 @@ for (let i in array) {
 const numbers = [1, 2, 3, 4, 5];
 const  squadNumbers = numbers.map((number) => number * number);
 console.log(squadNumbers);
+
+
+let task = [
+    {name: 'Tarea de matemmatica', duration: 120},
+    {name: 'Jugar videojuegos', duration: 60},
+    {name: 'Visitar a la abuela', duration: 240} 
+ 
+];
+
+let difficult_tasks = task.filter(task => task.duration >= 120);
+
+console.log(difficult_tasks);
